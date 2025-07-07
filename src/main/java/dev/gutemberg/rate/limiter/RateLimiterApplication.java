4de27a5ca2275422e9f1dc9c2bf10c7f85d2efbb
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-07-07 13:09:54 -0300

    init project

---
+++ src/main/java/dev/gutemberg/rate/limiter/RateLimiterApplication.java
@@ -0,0 +1,13 @@
+package dev.gutemberg.rate.limiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterApplication.class, args);
+	}
+
+}
