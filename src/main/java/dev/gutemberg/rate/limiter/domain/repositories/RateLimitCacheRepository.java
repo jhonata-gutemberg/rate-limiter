d28574280fbf9a407c68ee3f805d11225850ca9b
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-07-15 05:30:46 -0300

    rename rate limit repository to rate limit cache repository

--- src/main/java/dev/gutemberg/rate/limiter/domain/repositories/RateLimitCacheRepository.java
+++ src/main/java/dev/gutemberg/rate/limiter/domain/repositories/RateLimitCacheRepository.java
@@ -4,6 +4,6 @@ import dev.gutemberg.rate.limiter.domain.models.RateLimit;
 import dev.gutemberg.rate.limiter.domain.models.RateLimitCollectionKey;
 import java.util.Set;
 
-public interface RateLimitRepository {
+public interface RateLimitCacheRepository {
     Set<RateLimit> findAllByCollectionKey(RateLimitCollectionKey collectionKey);
 }
--- src/main/java/dev/gutemberg/rate/limiter/infrastructure/repositories/RedisRateLimitCacheRepository.java
+++ src/main/java/dev/gutemberg/rate/limiter/infrastructure/repositories/RedisRateLimitCacheRepository.java
@@ -3,20 +3,26 @@ package dev.gutemberg.rate.limiter.infrastructure.repositories;
 import dev.gutemberg.rate.limiter.domain.models.RateLimit;
 import dev.gutemberg.rate.limiter.domain.models.RateLimitCollectionKey;
 import dev.gutemberg.rate.limiter.domain.repositories.RateLimitRepository;
+import dev.gutemberg.rate.limiter.infrastructure.contracts.StringKeyConverter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 import java.util.Set;
 
 @Repository
 public class RedisRateLimitRepository implements RateLimitRepository {
+    private final StringKeyConverter<RateLimitCollectionKey> stringKeyConverter;
     private final RedisTemplate<String, RateLimit> redisTemplate;
 
-    public RedisRateLimitRepository(final RedisTemplate<String, RateLimit> redisTemplate) {
+    public RedisRateLimitRepository(
+            final StringKeyConverter<RateLimitCollectionKey> stringKeyConverter,
+            final RedisTemplate<String, RateLimit> redisTemplate
+    ) {
+        this.stringKeyConverter = stringKeyConverter;
         this.redisTemplate = redisTemplate;
     }
 
     @Override
     public Set<RateLimit> findAllByCollectionKey(final RateLimitCollectionKey collectionKey) {
-        return redisTemplate.opsForSet().members(collectionKey.toString());
+        return redisTemplate.opsForSet().members(stringKeyConverter.convert(collectionKey));
     }
 }
