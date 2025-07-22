5037e421550f812c8bc5ad6386930786fbcdf489
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-07-22 19:16:23 -0300

    implementing scheduler

--- src/main/java/dev/gutemberg/rate/limiter/domain/rate/limit/contracts/schedulers/TokenBucketRefillScheduler.java
+++ src/main/java/dev/gutemberg/rate/limiter/domain/rate/limit/contracts/schedulers/TokenBucketRefillScheduler.java
@@ -1,8 +1,7 @@
 package dev.gutemberg.rate.limiter.domain.rate.limit.contracts.schedulers;
 
 import dev.gutemberg.rate.limiter.domain.token.bucket.models.TokenBucketRefill;
-import java.time.Instant;
 
 public interface TokenBucketRefillScheduler {
-    void schedule(TokenBucketRefill refill, Instant nextRefillAt);
+    void schedule(TokenBucketRefill refill);
 }
