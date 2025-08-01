d016372061196c5b9e1ccdac5ed9e47b5b3e1a4f
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-01 17:55:15 -0300

    remove spring dependenvy from jobrunr

--- modules/infra/jobrunr/src/main/java/dev/gutemberg/rate/limiter/infra/jobrunr/JobRunrTokenBucketRefillScheduler.java
+++ modules/infra/jobrunr/src/main/java/dev/gutemberg/rate/limiter/infra/jobrunr/JobRunrTokenBucketRefillScheduler.java
@@ -4,20 +4,22 @@ import dev.gutemberg.rate.limiter.domain.rate.limit.contracts.schedulers.TokenBu
 import dev.gutemberg.rate.limiter.domain.rate.limit.models.RateLimitConfig.Limit.Unit;
 import dev.gutemberg.rate.limiter.domain.token.bucket.models.TokenBucketRefill;
 import dev.gutemberg.rate.limiter.domain.token.bucket.usecases.TokenBucketRefillUseCase;
+import jakarta.inject.Named;
 import org.jobrunr.scheduling.JobScheduler;
-import org.jobrunr.scheduling.cron.Cron;
-import org.springframework.stereotype.Component;
 import java.util.Map;
 
 import static dev.gutemberg.rate.limiter.domain.rate.limit.models.RateLimitConfig.Limit.Unit.*;
+import static org.jobrunr.scheduling.cron.Cron.minutely;
+import static org.jobrunr.scheduling.cron.Cron.hourly;
+import static org.jobrunr.scheduling.cron.Cron.daily;
 
-@Component
+@Named
 public class JobRunrTokenBucketRefillScheduler implements TokenBucketRefillScheduler {
     private static final Map<Unit, String> CRON = Map.of(
             SECOND, "* * * * * *",
-            MINUTE, Cron.minutely(),
-            HOUR, Cron.hourly(),
-            DAY, Cron.daily()
+            MINUTE, minutely(),
+            HOUR, hourly(),
+            DAY, daily()
     );
 
     private final JobScheduler jobScheduler;
