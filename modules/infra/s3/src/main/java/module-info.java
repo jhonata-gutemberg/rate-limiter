5db603341072517aa58834c8afcc6a08a38a5bcd
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-01 23:33:09 -0300

    improve s3 isolation with java modules

---
+++ modules/infra/s3/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module dev.gutemberg.rate.limiter.infra.s3 {
+    exports dev.gutemberg.rate.limiter.infra.s3.contracts;
+}
---
+++ modules/infra/slf4j/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module rate.limiter.modules.infra.slf4j.main {
+    requires dev.gutemberg.rate.limiter.infra.s3;
+    requires jakarta.inject;
+    requires org.slf4j;
+}
