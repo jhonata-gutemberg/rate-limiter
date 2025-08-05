a71a3d51a769a0d00aa7dabae05e1185a45d64b8
Jhonata Gutemberg <gutemberg.jhonata@gmail.com>
2025-08-05 18:54:20 -0300

    create s3 bucket with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,22 @@
 provider "aws" {
-  region = "us-west-2"
+  access_key = "local"
+  secret_key = "local"
+  region     = "us-west-2"
+
+  s3_use_path_style           = true
+  skip_requesting_account_id  = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+
+  endpoints {
+    s3  = "http://localhost:4566"
+    sts = "http://localhost:4566"
+  }
 }
 
 module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "5.2.0"
+
+  bucket = "ratelimiter-configs"
 }
